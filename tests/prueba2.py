037dac69dab77c646e4f60cd20e7041f4912f865
alejandrocastroarayaitcr <alejandrocastroaraya@estudiantec.cr>
2021-03-12 05:20:30 -0600

    Mejor backtracking pero ocupo arreglar la recursividad

--- tests/prueba2.py
+++ tests/prueba2.py
@@ -8,6 +8,8 @@ partes_del_cuerpo = ["cabeza", "pecho", "abdomen", "espalda", "piernas", "brazos
 lugares = ["sala", "comedor", "banno", "terraza", "cuarto", "garage", "patio", "balcon", "cocina"]
 
 cartas_juego = [sospechosos,armas,motivos,partes_del_cuerpo,lugares]
+cartas_correctas = []
+cartas_incorrectas = []
 
 encontrado = False
 
@@ -27,33 +29,42 @@ def get_cartas_iniciales():
 solucion_cartas = get_cartas_iniciales()
 
 
-def solucion_backtracking(cartas, solucion=[]):
+def solucion_backtracking(cartas, solucion):
     global encontrado
+    global cartas_incorrectas
+    global cartas_correctas
+    print(solucion)
+
     if encontrado == True:
         return
     solucion_temporal = []
 
     if len(solucion) == len(cartas_juego):
         if solucion == solucion_cartas:
-            print("La solucion ganadora es: ",solucion)
             print("------------------------------------------ WIN  ----------------------------------")
             encontrado = True
             return solucion
         else:
-            # print("LOSE ")
+            print("Combinación incorrecta")
             pass
-        print(solucion)
 
         solucion = []
     else:
         for i in cartas: # cada i es una categoria de las cartas del juego
             cartas = cartas[1:]
             for j in i: # cada j es un item perteneciente a una categoria
-                print(j)
+                #print(j)
                 if j in solucion_cartas: # Si el item es parte de la solucion
-                    print("Parte de la solucion encontrada es:",j)
-                    print(solucion + [j])
-                    solucion_backtracking(cartas, solucion + [j])
+                    #print("Parte de la solucion encontrada es:",j)
+                    #print(solucion + [j])
+                    if j not in cartas_correctas:
+                        cartas_correctas = cartas_correctas + [j]
+                        solucion_backtracking(cartas, solucion + [j])
+                else:
+                    if j not in cartas_incorrectas:
+                        print(solucion + [j])
+                        cartas_incorrectas = cartas_incorrectas + [j]
 
 
-print(solucion_backtracking(cartas_juego))
+
+print(solucion_backtracking(cartas_juego,[]))
