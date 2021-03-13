c8d4ef3e4c5aaf03a93b03f5eb22c68a6d8ea67f
alejandrocastroarayaitcr <alejandrocastroaraya@estudiantec.cr>
2021-03-13 06:19:59 -0600

    He agregado los tiempos de corrida y cambiado el nombre de prueba1 a bruteforce

--- tests/bruteforce.py
+++ tests/bruteforce.py
@@ -1,6 +1,5 @@
 import random
 import os
-import sys
 import time
 
 sospechosos =  ["amigo", "novio", "vecino", "mensajero", "extranno", "hermanastro", "colega de trabajo"]
@@ -13,23 +12,22 @@ cartas_juego = [sospechosos,armas,motivos,partes_del_cuerpo,lugares]
 cartas_correctas = []
 cartas_incorrectas = []
 parejas_restringidas = []
-
 encontrado = False
 
 
 def get_cartas_iniciales():
     """[obtenemos la combinacion de cartas iniciales que debemos buscar]
+
     Returns:
         [array]: [array con las cartas obtenidas]
-    """
+    """    
     new_cartas = []
     for elemento in cartas_juego:
         new_cartas += [random.choice(elemento)]
     print ("cartas: ",new_cartas)
     return new_cartas
 
-
-def solucion_backtracking(cartas, cartas_escogidas, solucion, parejas):
+def solucion_bruteforce(cartas, cartas_escogidas, solucion, parejas):
     global encontrado
     global cartas_incorrectas
     global cartas_correctas
@@ -37,44 +35,27 @@ def solucion_backtracking(cartas, cartas_escogidas, solucion, parejas):
 
     if encontrado == True:
         return
+    solucion_temporal = []
 
     if len(solucion) == len(cartas_juego):
         if solucion == cartas_escogidas:
             print(solucion)
-            print("------------------------------------------ WIN  ----------------------------------")
+            print ("------------------------------------------ WIN  ----------------------------------")
             encontrado = True
             return solucion
-
         else:
-            for x in solucion:
-                if x in cartas_incorrectas:
-                    return
-
-            for z in parejas_restringidas:
-                if all(item in solucion for item in z) == True: #si tiene una pareja que fue detectada como restringida entonces se retorna
-                    return
-
-            print(solucion)
-            randomIncorrecta = random.choice(solucion)
-
-            while randomIncorrecta in cartas_escogidas:
-                randomIncorrecta = random.choice(solucion)
-
-            cartas_incorrectas = cartas_incorrectas + [randomIncorrecta]
-            print("Combinacion incorrecta. Carta marcada como incorrecta (se descartaran futuras soluciones con esta carta):",randomIncorrecta)
-
-            for y in parejas: # y es una pareja de la lista de parejas
-                if all(item in solucion for item in y) == True: #si tiene ambos objetos de la pareja en la solucion entonces no es una solucion valida
-                    parejas_restringidas = parejas_restringidas + [y]
-                    print("Combinacion contiene pareja restringida (se descartan futuras soluciones con esta pareja):",y)
-                    return
-
+            #print("LOSE ")   
+            pass
+        print(solucion)
+        
         solucion = []
     else:
-        for i in cartas: # cada i es una categoria de las cartas del juego
+        for i in cartas:
             cartas = cartas[1:]
-            for j in i: # cada j es un item perteneciente a una categoria
-                solucion_backtracking(cartas, cartas_escogidas, solucion + [j], parejas)
+            for j in i:
+                
+                solucion_bruteforce(cartas, cartas_escogidas,solucion + [j],parejas)
+
 
 def crearParejas(numParejas, cartas_escogidas):
     parejas = []
@@ -109,13 +90,15 @@ def crearParejas(numParejas, cartas_escogidas):
     print("parejas:",parejas)
     return parejas
 
-def corrida_backtracking(numParejas):
+def corrida_bruteforce(numParejas):
     solucion_cartas = get_cartas_iniciales()
     parejas = crearParejas(numParejas, solucion_cartas)
 
-    print(solucion_backtracking(cartas_juego, solucion_cartas, [], parejas))
-
+    print(solucion_bruteforce(cartas_juego, solucion_cartas, [], parejas))
+    
 timeBefore = time.time_ns()
-corrida_backtracking(15)
+corrida_bruteforce(15)
 timeAfter = time.time_ns()
-print("Tiempo total de runtime:",timeAfter - timeBefore)
+totalTime = timeAfter-timeBefore
+print("Tiempo total de runtime:",totalTime)
+#input()
