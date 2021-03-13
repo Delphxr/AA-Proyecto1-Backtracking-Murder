d2276dd163793fda570706e623734c0562241536
alejandrocastroarayaitcr <alejandrocastroaraya@estudiantec.cr>
2021-03-13 04:58:26 -0600

    Valida que las parejas restringidas no invaliden la solucion, funcion crearParejas hecha para que el codigo este mas organizado, en teoria ya todo funciona para el algoritmo de backtracking

--- tests/backtracking.py
+++ tests/backtracking.py
@@ -27,10 +27,8 @@ def get_cartas_iniciales():
     print ("cartas: ",new_cartas)
     return new_cartas
 
-solucion_cartas = get_cartas_iniciales()
 
-
-def solucion_backtracking(cartas, solucion, parejas):
+def solucion_backtracking(cartas, cartas_escogidas, solucion, parejas):
     global encontrado
     global cartas_incorrectas
     global cartas_correctas
@@ -40,7 +38,7 @@ def solucion_backtracking(cartas, solucion, parejas):
         return
 
     if len(solucion) == len(cartas_juego):
-        if solucion == solucion_cartas:
+        if solucion == cartas_escogidas:
             print(solucion)
             print("------------------------------------------ WIN  ----------------------------------")
             encontrado = True
@@ -58,16 +56,16 @@ def solucion_backtracking(cartas, solucion, parejas):
             print(solucion)
             randomIncorrecta = random.choice(solucion)
 
-            while randomIncorrecta in solucion_cartas:
+            while randomIncorrecta in cartas_escogidas:
                 randomIncorrecta = random.choice(solucion)
 
             cartas_incorrectas = cartas_incorrectas + [randomIncorrecta]
-            print("Incorrecta. Marcada:",randomIncorrecta)
+            print("Combinacion incorrecta. Carta marcada como incorrecta (se descartaran futuras soluciones con esta carta):",randomIncorrecta)
 
             for y in parejas: # y es una pareja de la lista de parejas
                 if all(item in solucion for item in y) == True: #si tiene ambos objetos de la pareja en la solucion entonces no es una solucion valida
                     parejas_restringidas = parejas_restringidas + [y]
-                    print("Contiene pareja restringida:",y)
+                    print("Combinacion contiene pareja restringida (se descartan futuras soluciones con esta pareja):",y)
                     return
 
         solucion = []
@@ -75,11 +73,12 @@ def solucion_backtracking(cartas, solucion, parejas):
         for i in cartas: # cada i es una categoria de las cartas del juego
             cartas = cartas[1:]
             for j in i: # cada j es un item perteneciente a una categoria
-                solucion_backtracking(cartas, solucion + [j], parejas)
+                solucion_backtracking(cartas, cartas_escogidas, solucion + [j], parejas)
 
-def corrida_backtracking(numParejas):
-
+def crearParejas(numParejas, cartas_escogidas):
     parejas = []
+    fullPareja = []
+
     while numParejas != 0:
         cartasPareja = random.choice(cartas_juego)
         pareja1 = random.choice(cartasPareja)
@@ -90,12 +89,29 @@ def corrida_backtracking(numParejas):
         while pareja1 == pareja2:
             pareja2 = random.choice(cartasPareja)
 
-        parejas = parejas + [[pareja1] + [pareja2]]
+        fullPareja = [[pareja1] + [pareja2]]
 
+        while all(item in cartas_escogidas for item in fullPareja) == True: #si la solucion contiene las cartas de la pareja formada, entonces se crea otra pareja hasta que la pareja no invalide la solucion
+            print("La pareja no puede contradecir a la solucion. Pareja que contradecia solucion:", fullPareja)
+            cartasPareja = random.choice(cartas_juego)
+            pareja1 = random.choice(cartasPareja)
+            cartasPareja = random.choice(cartas_juego)
+            pareja2 = random.choice(cartasPareja)
+            while pareja1 == pareja2:
+                pareja2 = random.choice(cartasPareja)
+            fullPareja = [[pareja1] + [pareja2]]
+            print("La nueva pareja es:",fullPareja)
+
+        parejas = parejas + fullPareja
         numParejas = numParejas - 1
 
     print("parejas:",parejas)
+    return parejas
 
-    print(solucion_backtracking(cartas_juego, [], parejas))
+def corrida_backtracking(numParejas):
+    solucion_cartas = get_cartas_iniciales()
+    parejas = crearParejas(numParejas, solucion_cartas)
+
+    print(solucion_backtracking(cartas_juego, solucion_cartas, [], parejas))
 
 corrida_backtracking(15)
